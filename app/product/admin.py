d6e52b95b45b51c1c388ef18cc252e31db26d27d
LaurinJ <laurin223@seznam.cz>
2020-10-27 23:23:16 +0100

    Model category and product created

--- app/product/admin.py
+++ app/product/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.product.models import Category
+
+admin.site.register(Category)
--- app/product/models.py
+++ app/product/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+def category_image_url(instance, filename):
+    '''upload_to for category image'''
+    return 'category_{0}/{1}'.format(instance.id, filename)
+
+def product_image_url(instance, filename):
+    '''upload_to for category image'''
+    return 'product_{0}/{1}'.format(instance.id, filename)
+
+class Category(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to=category_image_url)
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to=product_image_url)
+    price = models.FloatField()
+    amount = models.IntegerField()
+    minamount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
